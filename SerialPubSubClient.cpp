aa0e9a88bd7c2fc5925dd3f80b2af2556a978d9a
Paolo Bueno <paolohaji@gmail.com>
2017-10-23 19:42:53 -0200

    Fixes

--- SerialPubSubClient.cpp
+++ SerialPubSubClient.cpp
@@ -1,71 +1,67 @@
 #include "SerialPubSubClient.h"
 PubSubClient::PubSubClient(IPAddress ip, uint16_t port, MQTT_CALLBACK_SIGNATURE, Client& client) {
-	return setCallback(callback);
+  setCallback(callback);
 }
 
 PubSubClient::PubSubClient(const char* domain, uint16_t port, MQTT_CALLBACK_SIGNATURE, Client& client) {
-	return setCallback(callback);
+  setCallback(callback);
 }
 
-PubSubClient::PubSubClient(MQTT_CALLBACK_SIGNATURE) {
-	return setCallback(callback);
+PubSubClient& PubSubClient::setCallback(MQTT_CALLBACK_SIGNATURE) {
+  this->callback = callback;
+  return *this;
 }
 
-PubSubClient::setCallback(MQTT_CALLBACK_SIGNATURE) {
-	this->callback = callback;
-	return *this;
+boolean PubSubClient::connect(const char* id) {
+  return true;
 }
 
-PubSubClient::connect() {
-	return true;
-}
-PubSubClient::connect(const char* id) {
-	return true;
-}
-PubSubClient::connect(const char* id, const char* user, const char* pass) {
-	return true;
+boolean PubSubClient::connect(const char* id, const char* user, const char* pass) {
+  return true;
 }
 
 boolean PubSubClient::publish(const char* topic, const char* payload) {
-	return publish(topic, payload, false);
+  return publish(topic, payload, false);
 }
 
-boolean PubSubClient::publish(const char* topic, const char* payload) {
-	return publish(topic, payload, false);
-}
 boolean PubSubClient::publish(const char* topic, const char* payload, boolean retained) {
-	Serial.print("Publishing to topic :"); Serial.println(topic);
-	Serial.print("payload: "); Serial.println(payload);
-	Serial.print("retained: "); Serial.println(retained);
-	return true;
+  Serial.print("Publishing to topic :"); Serial.println(topic);
+  Serial.print("payload: "); Serial.println(payload);
+  Serial.print("retained: "); Serial.println(retained);
+  return true;
 }
 
-PubSubClient::subscribe(const char* topic, uint8_t qos) {
-	Serial.print("Subscribing to topic: "); Serial.print(topic); Serial.print("with QoS "); Serial.println(qos);
+boolean PubSubClient::subscribe(const char* topic, uint8_t qos) {
+  Serial.print("Subscribing to topic: "); Serial.print(topic); Serial.print("with QoS "); Serial.println(qos);
+  return true;
+}
+
+boolean PubSubClient::subscribe(const char* topic) {
+  return subscribe(topic, 0);
 }
 
 boolean PubSubClient::loop() {
-	if(Serial.available()) {
-		while (Serial.available()) {
-			String topic = Serial.readUntil('|');
-			String payload = Serial.readUntil('\n');
-			Serial.print("Receiving message on topic "); Serial.println(topic);
-			Serial.print("payload: "); Serial.println(payload);
-			if (callback) {
-				callback(topic.c_str(), payload.c_str(), payload.length());
-			}
-		}
-	}
-	return connected();
+  if(Serial.available()) {
+    while (Serial.available()) {
+      String topic = Serial.readStringUntil('|');
+      String payload = Serial.readStringUntil('\n');
+      Serial.print("Receiving message on topic "); Serial.println(topic);
+      Serial.print("payload: "); Serial.println(payload);
+      if (this->callback) {
+        callback(topic.c_str(), payload.c_str(), payload.length());
+      }
+    }
+  }
+  return connected();
 }
 
 boolean PubSubClient::connected() {
-	return !!Serial;
+  return !!Serial;
 }
 
 void PubSubClient::disconnect() {
 }
 
 int state() {
-	return 0;
+  return 0;
 }
--- SerialPubSubClient.h
+++ SerialPubSubClient.h
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <Serial.h>
 #include "Client.h"
 #include "Stream.h"
 #ifndef PubSubClient_h
@@ -9,32 +8,32 @@
 
 class PubSubClient {
 private:
-	callback;
+  MQTT_CALLBACK_SIGNATURE;
 public:
-   PubSubClient();
-   PubSubClient(IPAddress, uint16_t, MQTT_CALLBACK_SIGNATURE,Client& client);
-   PubSubClient(uint8_t *, uint16_t, MQTT_CALLBACK_SIGNATURE,Client& client);
-   PubSubClient(uint8_t *, uint16_t, MQTT_CALLBACK_SIGNATURE,Client& client, Stream&);
-   PubSubClient(const char*, uint16_t, Client& client);
-   PubSubClient(const char*, uint16_t, Client& client, Stream&);
-   PubSubClient(const char*, uint16_t, MQTT_CALLBACK_SIGNATURE,Client& client);
-   PubSubClient(const char*, uint16_t, MQTT_CALLBACK_SIGNATURE,Client& client, Stream&);
-
-   PubSubClient& setCallback(MQTT_CALLBACK_SIGNATURE);
-   PubSubClient& setClient(Client& client);
-   PubSubClient& setStream(Stream& stream);
-
-   boolean connect(const char* id);
-   boolean connect(const char* id, const char* user, const char* pass);
-   void disconnect();
-   boolean publish(const char* topic, const char* payload);
-   boolean publish(const char* topic, const char* payload, boolean retained);
-   boolean subscribe(const char* topic);
-   boolean subscribe(const char* topic, uint8_t qos);
-   boolean unsubscribe(const char* topic);
-   boolean loop();
-   boolean connected();
-   int state();
+  PubSubClient();
+  PubSubClient(IPAddress, uint16_t, MQTT_CALLBACK_SIGNATURE,Client& client);
+  PubSubClient(uint8_t *, uint16_t, MQTT_CALLBACK_SIGNATURE,Client& client);
+  PubSubClient(uint8_t *, uint16_t, MQTT_CALLBACK_SIGNATURE,Client& client, Stream&);
+  PubSubClient(const char*, uint16_t, Client& client);
+  PubSubClient(const char*, uint16_t, Client& client, Stream&);
+  PubSubClient(const char*, uint16_t, MQTT_CALLBACK_SIGNATURE,Client& client);
+  PubSubClient(const char*, uint16_t, MQTT_CALLBACK_SIGNATURE,Client& client, Stream&);
+  
+  PubSubClient& setCallback(MQTT_CALLBACK_SIGNATURE);
+  PubSubClient& setClient(Client& client);
+  PubSubClient& setStream(Stream& stream);
+  
+  boolean connect(const char* id);
+  boolean connect(const char* id, const char* user, const char* pass);
+  void disconnect();
+  boolean publish(const char* topic, const char* payload);
+  boolean publish(const char* topic, const char* payload, boolean retained);
+  boolean subscribe(const char* topic);
+  boolean subscribe(const char* topic, uint8_t qos);
+  boolean unsubscribe(const char* topic);
+  boolean loop();
+  boolean connected();
+  int state();
 };
 
 
